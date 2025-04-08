d122c7258f331ab3d96017dc85b0d763a6a784fd
Mario Portilho <junior.portilho2005@gmail.com>
2025-04-08 15:55:58 +0100

    organized repo

--- client/main.py
+++ client/main.py
@@ -4,7 +4,7 @@ n_requests = 50
 
 
 for _ in range(n_requests):
-    x = requests.get('http://localhost:1338')
+    x = requests.get('http://158.220.93.168')
 
     j = x.json()
     print(j["message"])
